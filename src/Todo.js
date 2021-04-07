97fc9aa9bc053900a9ba8443dbce7bdfd7637635
Chandni <chandni1141996@gmail.com>
2021-04-07 17:15:31 +0530

    deleting task

--- src/Todo.js
+++ src/Todo.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react';
 
 class Todo extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+
+    handleRemove() {
+        this.props.removeTodo(this.props.id);
+    }
+
+    handleEdit() {
+        this.props.editTodo(this.props.id);
+    }
+
     render() {
         let {task} = this.props;
-
+        
         return(
             <div>
                 <div>{task}</div>
+                <button onClick = {this.handleRemove}>Delete</button>
+                <button onClick = {this.handleEdit}>Edit</button>
             </div>
         )
     }
--- src/TodoList.js
+++ src/TodoList.js
@@ -8,18 +8,23 @@ class TodoList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            tasks : [{ task : "Take bath", key : uuidv4()}]
+            tasks : []
         }
         
         this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
     }
 
     addTodo(task) {
         this.setState({tasks : [...this.state.tasks, {task, key : uuidv4()} ] });
     }
 
+    removeTodo(id) {
+        this.setState({tasks : this.state.tasks.filter( t => t.key !== id)});
+    }
+
     render() {
-        const tasks = this.state.tasks.map(t => <Todo task = {t.task} key = {t.key} />);
+        const tasks = this.state.tasks.map(t => <Todo task = {t.task} removeTodo = {this.removeTodo} key = {t.key} id = {t.key} />);
         return(
             <div>
                 <h1>TodoList</h1>
