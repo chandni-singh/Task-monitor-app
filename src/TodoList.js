0a9c1808d49edd0a030680e2b3efd76bf33bd563
Chandni <chandni1141996@gmail.com>
2021-04-07 13:54:57 +0530

    starter records

---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component {
+    render() {
+        return(
+            <h1>Hi from todo list!</h1>
+        )
+    }
+}
+
+export default TodoList;
